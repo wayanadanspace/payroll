41d75a25648f2bf83262bea3be7454e3e24210df
Gurunath <gurunath@amazeinc.in>
2023-04-14 14:22:27 +0530

    changes in componentdashboard

--- pages/Settings/componentmappingdashboard.js
+++ pages/Settings/componentmappingdashboard.js
@@ -1,9 +1,47 @@
-import React, { useState } from "react";
 import Styles from "../../styles/employmentJobHistory.module.css";
 import Link from "next/link";
 import Layout from "@/Components/layout";
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import Swal from "sweetalert2";
 export default function componentMappingDashboard() {
-  // const [modalOpen, setModalOpen] = useState(false);
+
+
+  const hostURL = process.env.NEXT_PUBLIC_API_HOST_URL;
+
+
+  const [componentMapping, setcomponentMapping] = useState([]);
+
+  const getcomponentMapping = async () => {
+    let res = await axios.get(hostURL + "Payroll/GetComponentMapping");
+    setcomponentMapping(res.data);
+  }
+
+  useEffect(() => {
+    getcomponentMapping()
+  }, [1])
+
+
+  const getData = (data) => {
+    sessionStorage.setItem("id", data.id);
+  }
+
+  const clearData = () => {
+    sessionStorage.setItem("id", "");
+  }
+
+
+  const handleDelete = async (id) => {
+    try {
+      let res = await axios.get(hostURL + `Payroll/DeleteComponentMapping?id=${id}`);
+      console.log(res.data);
+      Swal.fire('Data deleted successfully')
+      getcomponentMapping();
+    } catch (error) {
+      console.error(error);
+      Swal.fire('failed to  delete data')
+    }
+  };
 
   return (
     <Layout>
@@ -37,9 +75,8 @@ export default function componentMappingDashboard() {
             <div className="col-lg-2">
 
 
-              <Link href="/Settings/componentmapping"><button
-                className="btn btn-primary btn-sm shadow-lg"
-                id={Styles.addNew} > ADD new</button>
+              <Link href="/Settings/componentmapping"><button className="btn btn-primary btn-sm shadow-lg"
+                id={Styles.addNew} onClick={clearData.bind(this)} > ADD new</button>
                 {/* // onClick={() => setModalOpen(!modalOpen)}
 >   */}
                 {/* <AiOutlinePlusCircle /> */}
@@ -56,28 +93,30 @@ export default function componentMappingDashboard() {
                   <th>PayrollComponentType</th>
                   <th>Code</th>
                   <th>Component Name</th>
-                  <th>Enable</th>
                   <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>test</td>
-                  <td>
-                    <div className="row">
-                      <div className="col-lg-4">
-                        <button id={Styles.actionBtn}>Edit</button>
-                      </div>
 
-                      <div className="col-lg-4">
-                        <button id={Styles.actionBtn}>Delete</button>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
+                {componentMapping.map((data, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{data.payrollComponentType}</td>
+                      <td>{data.code}</td>
+                      <td>{data.componentName}</td>
+                      <td>
+                        <Link href="/Settings/componentmapping">
+                          <button className={Styles.actionButton} onClick={getData.bind(this, data)} >Edit</button>
+                        </Link>
+                        &nbsp;
+
+                        <button className={Styles.actionButton} onClick={() => handleDelete(data.id)}>Delete</button>
+                      </td>
+                    </tr>
+                  )
+                })
+                }
+
               </tbody>
             </table>
           </div>
